31275ece80aa80e30724a733bbe3f123f1f5938a
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 22:21:01 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -3,12 +3,7 @@ void main() {
   var num1=[1,2,3,4,5];
   var num2=[4,5,8,2,10];
 
-  var set1=num1.toSet();
-  var set2=num2.toSet();
+    bool subset= num1.contains(num2);
 
-  var different=set1.difference(set2);
-
-  var difflist=different.toList();
-
-  print(difflist);
+    print(subset);
 }
