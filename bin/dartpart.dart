befdf78e66f26f543e70112b6f5a52a44a67d432
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 18:56:51 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -1,7 +1,9 @@
 void main(){
+ //Find the largest number
 
    var list1=[1,2,3,4,5,6,7,8,9,10];
-   var rev=list1.reversed;
 
-   print(rev);
+   var large=list1.reduce((a,b)=> a>b? a:b);
+
+   print(large);
 }
