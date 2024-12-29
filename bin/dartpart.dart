52ad8bcb57f6456df80758e1a656277016b8bf27
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 22:06:46 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -3,7 +3,7 @@ void main() {
   var num1=[1,2,3,4,5];
   var num2=[4,5,8,2,10];
 
-  var intersection= num1.where((item)=> num2.contains(item)).toList();
+  var union= num1.toSet().union(num2.toSet()).toList();
 
-  print(intersection);
+  print(union);
 }
