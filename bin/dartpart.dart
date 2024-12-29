fb458fe99cabd24681775ed109acfe106f7e39b4
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 19:22:37 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -1,9 +1,9 @@
 void main(){
- //Even number in list
+ //Odd number in list
 
    var list1=[1,10,15,4,8,6,7,2,9,13,11];
 
-    var even= list1.where((number)=> number%2==0).toList();
+    var odd= list1.where((number)=> number%2!=0).toList();
 
-   print(even);
+   print(odd);
 }
