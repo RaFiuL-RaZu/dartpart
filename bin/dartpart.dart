39dda6792b2e894889271de954a9f830336b7308
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 19:46:58 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -1,9 +1,9 @@
 void main(){
  //multiple by 2 in list number
 
-   var list1=[10,20,25,30,40,45];
+   var list1=[10,20,25,30,40,45,50,55,60];
 
-  var firstElement= list1.sublist(2,4);
-  print(firstElement);
+  var sublistinlist= list1.sublist(0,6);
+  print(sublistinlist);
 
 }
