55614ce2f67b943e3cfa29ae948b09a58b6d23c1
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 19:03:50 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -1,9 +1,9 @@
 void main(){
- //sum the number in list
+ //Avarege of the number in list
 
    var list1=[1,2,3,4,5,6,7,8,9,10];
 
    int sum=list1.fold(0,(previous, element)=> previous+element);
-
-   print("The Number of sum is:$sum");
+  double avarage=sum/list1.length;
+   print("Avarage Number OF = $avarage");
 }
