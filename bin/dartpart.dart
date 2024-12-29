a02e27e03d542dcbf5778728c68ad8af97540ba2
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 22:15:01 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -3,7 +3,12 @@ void main() {
   var num1=[1,2,3,4,5];
   var num2=[4,5,8,2,10];
 
-  var union= num1.toSet().union(num2.toSet()).toList();
+  var set1=num1.toSet();
+  var set2=num2.toSet();
 
-  print(union);
+  var different=set1.difference(set2);
+
+  var difflist=different.toList();
+
+  print(difflist);
 }
