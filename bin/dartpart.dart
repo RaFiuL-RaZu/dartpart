78b50bad230d56c73b8f5952aef9db08de15f94b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 19:05:39 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -1,9 +1,8 @@
 void main(){
- //Avarege of the number in list
+ //contains element in list
 
    var list1=[1,2,3,4,5,6,7,8,9,10];
 
-   int sum=list1.fold(0,(previous, element)=> previous+element);
-  double avarage=sum/list1.length;
-   print("Avarage Number OF = $avarage");
+   var cont=list1.contains(5);
+   print(cont);
 }
