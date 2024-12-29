61b6680b9ea76e6d7e842dcfd7caecd37d7ef068
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-29 21:47:16 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -1,15 +1,11 @@
 
 void main() {
-  var numbers = [1, 2, 3, 4, 5];
+  var numbers=[1,2,3,4,5];
+  var last=numbers[numbers.length-1];
 
-  int last = numbers[numbers.length - 1];
-
-  for (int i = numbers.length - 1; i > 0; i--) {
-    numbers[i] = numbers[i - 1];
+  for(int i=numbers.length-1; i>0;i--){
+    numbers[i]=numbers[i-1];
   }
-
-
-  numbers[0] = last;
-
+  numbers[0]=last;
   print(numbers);
 }
