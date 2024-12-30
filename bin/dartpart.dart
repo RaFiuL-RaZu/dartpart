4f04172e8bd13d688d6212713198bfd844a64941
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-30 09:36:21 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -2,18 +2,16 @@ import 'dart:io';
 
 void main(){
 
-  stdout.write("Enter Principal Amount:");
-  double Pricipal= double.parse(stdin.readLineSync()!);
+  stdout.write("Enter Your Number:");
+  double Num= double.parse(stdin.readLineSync()!);
 
-  stdout.write("Enter Rate Amount:");
-  double Rate= double.parse(stdin.readLineSync()!);
-
-  stdout.write("Enter Time (in years):");
-  double Time= double.parse(stdin.readLineSync()!);
-
-  var simpleInterest= (Pricipal*Rate*Time)/100;
-
-  print(simpleInterest);
-
+  bool isPrime= true;
+  for(int i=2;i<=Num~/2;i++){
+    if(Num%2==0){
+      isPrime=false;
+      break;
+    }
+  }
+  print(isPrime? "Prime": "Not Prime");
 
 }
