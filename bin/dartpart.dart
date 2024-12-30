75b042d2083ecfb511267675091cf453051e8352
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-30 09:38:56 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -5,13 +5,12 @@ void main(){
   stdout.write("Enter Your Number:");
   double Num= double.parse(stdin.readLineSync()!);
 
-  bool isPrime= true;
-  for(int i=2;i<=Num~/2;i++){
-    if(Num%2==0){
-      isPrime=false;
-      break;
-    }
+  int Fact=1;
+
+  for(int i=1;i<=Num;i++){
+    Fact*=i;
   }
-  print(isPrime? "Prime": "Not Prime");
+
+  print(Fact);
 
 }
