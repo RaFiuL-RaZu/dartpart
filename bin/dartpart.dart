a16625db0e1d95af87fe4d181788d36e583eb2eb
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-30 11:04:06 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -1,8 +1,19 @@
 
 
-void main() {
-  int a=10,b=25,c=20;
+import 'dart:io';
 
-  int max= (a>b)? (a>c? a:c):(b>c? b:c);
-  print(max);
+void main() {
+
+  stdout.write("Enter your Number :");
+  int num= int.parse(stdin.readLineSync()!);
+
+  int rev=0;
+
+  while(num!=0){
+    int digit=num%10;
+    rev=rev*10+digit;
+    num~/=10;
+  }
+
+  print("Reverse Number is: $rev");
 }
