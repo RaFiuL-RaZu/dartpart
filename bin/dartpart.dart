288d38808a8a93f6c483f8262e227896f2a0e74b
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-30 09:43:33 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -5,12 +5,14 @@ void main(){
   stdout.write("Enter Your Number:");
   double Num= double.parse(stdin.readLineSync()!);
 
-  int Fact=1;
+ int a=0, b=1;
 
-  for(int i=1;i<=Num;i++){
-    Fact*=i;
-  }
+ for(int i=1; i<=Num;i++){
+   print(a);
+   int fibo=a+b;
+   a=b;
+   b=fibo;
 
-  print(Fact);
+ }
 
 }
