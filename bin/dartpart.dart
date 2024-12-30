23ec201f06c9e80eaf7d0f87f7956d2c54316e58
RaFiuL-RaZu <rafi102621@gmail.com>
2024-12-30 11:37:01 +0600

    innit commit

--- bin/dartpart.dart
+++ bin/dartpart.dart
@@ -4,14 +4,16 @@ import 'dart:io';
 
 void main() {
 
-  stdout.write("Enter your Number :");
-  int num= int.parse(stdin.readLineSync()!);
+  int num=20;
 
-  int a=0,b=1;
-  for(int i=0;i<num;i++){
-    print(a);
-    int next=a+b;
-    a=b;
-    b=next;
+  bool isprime=true;
+  
+  for(int i=0; i<num~/2;i++){
+    if(num%2==0){
+      isprime=false;
+      break;
+    }
+
   }
+  print(isprime? "Prime": "Not prime");
 }
